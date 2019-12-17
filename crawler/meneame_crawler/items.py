67b18faaa0e1acd4a09e72d5e22f3ac5730625c4
micobionte <micobionte@localhost.localdomain>
2019-12-17 23:52:48 +0100

    Fixed typo

--- crawler/meneame_crawler/items.py
+++ crawler/meneame_crawler/items.py
@@ -7,8 +7,27 @@
 
 import scrapy
 
-
 class MeneameCrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class MnmTestItem(scrapy.Item):
+	index = scrapy.Field()
+	noticia = scrapy.Field()
+	link_noticia = scrapy.Field()
+	web = scrapy.Field()
+	usuario = scrapy.Field()
+	id_usuario = scrapy.Field()
+	fecha_envio = scrapy.Field()
+	fecha_publicacion = scrapy.Field()
+	meneos = scrapy.Field()
+	clicks = scrapy.Field()
+	comentarios = scrapy.Field()
+	votos_positivos = scrapy.Field()
+	votos_anonimos = scrapy.Field()
+	votos_negativos = scrapy.Field()
+	karma = scrapy.Field()
+	sub = scrapy.Field()
+	extracto = scrapy.Field()
+
